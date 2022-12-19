ccee583c5cc91d516c3c7d93e7efb9fa72765164
Tricia Amheiser <tricia@taarts.net>
2022-12-19 17:19:55 -0500

    Adjusted footer content

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import { useQuery } from 'react-query'
 // import { getCryptoData } from './api'
 
 type CryptoDataType = {
-  id: string
-  rank: string
+  id: number
+  rank: number
   symbol: string | null
   name: string | null
-  supply: string | null
+  supply: number
   maxSupply: string | null
-  marketCapUsd: string | null
-  volumeUsd24Hr: string | null
-  priceUsd: string | null
-  changePercent24Hr: string | null
+  marketCapUsd: number
+  volumeUsd24Hr: number
+  priceUsd: number
+  changePercent24Hr: number
   vwap24Hr: string | null
-  explorer: string | null
+  explorer: string
 }
 
 export function App() {
@@ -22,29 +22,35 @@ export function App() {
   const [cycle, setCycle] = useState<number>(0)
   console.log('cryptoData', cryptoData)
 
-  // const  { data: cryptoData= [], refetch: refetchCryptoData } = useQuery('cryptoData', getCryptoData())
-
   function loadCryptoData() {
     async function getCryptoData() {
       const response = await fetch('https://api.coincap.io/v2/assets')
       if (response.ok) {
-        const data = await response.json()
-        return data.data
+        const { data } = await response.json()
+        setCryptoData(data)
+        // return data.data
       }
     }
     getCryptoData()
   }
-  console.log('loadCryptoData', [cycle])
+  console.log('getCryptoData', [cycle])
 
   useEffect(function () {
     const interval = setInterval(function () {
       setCycle((cycle) => cycle + 1)
-    }, 10000)
+    }, 1000)
     return () => clearInterval(interval)
   }, [])
 
   useEffect(loadCryptoData, [cycle])
 
+  function roundingCrypto(number: number) {
+    return Math.round(number * 100) / 1000
+
+    if (number - Math.floor(number) === 0) {
+      return
+    }
+  }
   return (
     <>
       <div className="app">
@@ -52,21 +58,47 @@ export function App() {
           <h1>Crypto Ticker</h1>
         </header>
         <main>
-          <h2 className="subheader">Currency</h2>
-          <ul>
-            {cryptoData.map(function (crypto: CryptoDataType) {
-              return (
-                <li className="crypto" key={crypto.id}>
-                  <span>{crypto.rank}</span>
-                  <span>{crypto.symbol}</span>
-                  <span>{crypto.name}</span>
-                  <span>{crypto.supply}</span>
-                  <span>{crypto.priceUsd}</span>
-                </li>
-              )
-            })}
-          </ul>
+          <h2 className="subheader">Track Price</h2>
+          <table>
+            <tr className="headings">
+              <th>Rank</th>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Market Cap</th>
+              <th>Supply</th>
+              <th>$Price</th>
+              <th>%Change</th>
+              <th>Link</th>
+            </tr>
+            {cryptoData
+              .filter((crypto) => crypto.rank <= 25)
+              .map(function (crypto) {
+                return (
+                  <tr className="crypto" key={crypto.id}>
+                    <td className="spacing">{crypto.rank}</td>
+                    <td>{crypto.symbol}</td>
+                    <td>{crypto.name}</td>
+                    <td className="right">
+                      {roundingCrypto(crypto.marketCapUsd)}
+                    </td>
+                    <td className="right">{roundingCrypto(crypto.supply)}</td>
+                    <td className="right">{roundingCrypto(crypto.priceUsd)}</td>
+                    <td className="right">
+                      {roundingCrypto(crypto.changePercent24Hr)}
+                    </td>
+                    <td key="explorer">
+                      <a href={crypto.explorer}>
+                        <i className="bx bx-link-external"></i>
+                      </a>
+                    </td>
+                  </tr>
+                )
+              })}
+          </table>
         </main>
+        <footer>
+          <div className="box">by T AMHEISER</div>
+        </footer>
       </div>
     </>
   )
